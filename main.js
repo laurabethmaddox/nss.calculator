da090e137bc5124ca98d7f30f2bfade2f85ce8c0
Laura Beth Maddox <laurabethmaddox@gmail.com>
2022-01-14 11:39:33 -0600

    Calculator commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const subtract = (firstNumber, secondNumber) => {
+    const difference = firstNumber - secondNumber
+    return difference
+}
+
+let theDifference = subtract()
+console.log(theDifference)
+
+const divide = (firstNumber, secondNumber) => {
+    const quotient = firstNumber / secondNumber
+    return quotient
+}
+
+let theQuotient = divide()
+console.log(theQuotient)
+
+const multiply = (firstNumber, secondNumber) => {
+    const product = firstNumber * secondNumber
+    return product
+}
+
+let theProduct = multiply()
+console.log(theProduct)
+
+const square = (originalNumber) => {
+    const squared = originalNumber * originalNumber
+
+    return squared
+}
+
+let theSquared = square()
+console.log(theSquared)
